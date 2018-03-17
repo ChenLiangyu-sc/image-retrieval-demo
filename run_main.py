1852a903ce7724658ede224c9057038d46961759
sanqianshuijun <525876914@qq.com>
2018-03-17 20:11:23 +0800

    Update run_main.py

--- run_main.py
+++ run_main.py
@@ -4,7 +4,7 @@ import sys
 
 def main(args):
     retrieve_demo=retrieve_demo_gpu.image_retrieval(args = args)
-    retrieve_demo.retrieval(path = args.retrieve_image_dir)
+    retrieve_demo.retrieval(path = args.initialize_image_dir)
     for dir in range(1000):
         try:
             print('Enter the picture path')
@@ -21,16 +21,20 @@ def parse_arguments(argv):
     help='Load a pretrained model before training starts.',default='./model1.55293.ckpt-18006')
 
     parser.add_argument('--initialize_image_dir', type=str,
-    help='initialization.', default='/data1/chenliangyu/data/33/201330000071/200330588058.jpg')
-
-    parser.add_argument('--retrieve_image_dir', type=str,
-    help='retrieve image', default='/data1/chenliangyu/data/33/201330000071/200330588058.jpg')
+    help='Image path for initialization.', default='')
 
     parser.add_argument('--image_mean_dir', type=str,
     help='Image mean', default='./mean_227.npy')
 
-
-
+    parser.add_argument('--dict_idna', type=str,
+    help='Image ID and name dictionary path', default='./dict.npy')
+    
+    parser.add_argument('--img_id', type=str,
+    help='Picture path address', default='./img_id.npy')
+    
+    parser.add_argument('--gpu_id', type=str,
+    help='use GPU with ID', default='1')
+    
     return parser.parse_args(argv)
 
 
