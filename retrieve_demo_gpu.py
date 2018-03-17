490f08570732b955c644a97a3654e03a1b188564
sanqianshuijun <525876914@qq.com>
2018-03-17 19:34:40 +0800

    Update retrieve_demo_gpu.py

--- retrieve_demo_gpu.py
+++ retrieve_demo_gpu.py
@@ -2,37 +2,35 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-#import system things
+
 
 import tensorflow as tf
 import numpy as np
 import os
-#import pandas as pd
+
 import time
-#import datetime
-#from datetime import datetime
+
 import cv2
 
-#import read_data_youpiao as read_data
 import inference_youpiao as inference2
 import count_dist as count
-#import read_data_pool as read_data1
-#import test111 as test
+
 
 
 class image_retrieval:
 
-    def __init__(self):
-        
+    def __init__(self, args):
+        self.args = args
         config = tf.ConfigProto()
-        
+        os.environ["CUDA_VISIBLE_DEVICES"] = '5' #use GPU with ID=0
         config.gpu_options.allow_growth = True
-        #config.gpu_options.per_process_gpu_memory_fraction = 
+
         self.sess = tf.Session(config=config)
         self.siamese = inference2.siamese()
         config.gpu_options.allow_growth = True
         
         self.count = count.count()
+
         with self.count.g1.as_default():
             self.sess1 = tf.Session(graph=self.count.g1)
             self.sess1.run(tf.global_variables_initializer())
@@ -47,12 +45,12 @@ class image_retrieval:
             self.sess4.run(tf.global_variables_initializer())              
         self.sess.run(tf.global_variables_initializer())
         saver = tf.train.Saver()
-        saver.restore(self.sess,'./model1.55293.ckpt-18006')
+        saver.restore(self.sess, self.args.pretrained_model)
 
 
 
 
-        aaa = np.load('mean_227.npy')
+        aaa = np.load(args.image_mean_dir)
         bbb = []
         aaa = aaa.tolist()
         for _ in range(1):
@@ -65,15 +63,16 @@ class image_retrieval:
         self.img_id = np.load('./img_id.npy')
         dict = np.load('./dict.npy')
         self.dict = dict.tolist()
-        path, _ = self.retrieval(path = '/data3/chenliangyu/data/youpiao/jiteyoupiao/T89/(3-2)/1502431789341.png')
+        path, _ = self.retrieval(path = self.args.initialize_image_dir)
 
 
     def retrieval(self,path):
         image=cv2.imread(path)
-        image=cv2.resize(image,(227,227))
+        
+        image=cv2.resize(image, (227,227))
         img = image[np.newaxis]
         img = img - self.aaa
-        #retrieved_img_feat = self.siamese.network(img)
+
         start_time = time.time()
 
         self.retrieved_img_feat, self.predictions = self.sess.run([self.siamese.retrieved_img_feat1, self.siamese.predictions], feed_dict={
@@ -110,11 +109,12 @@ class image_retrieval:
         dist = np.concatenate((dist123,dist4[0]),axis = 0)
 
         sortedIndex_mat=np.argsort(dist)
-        img_id2 = [str(img_id1, encoding = "utf-8") for img_id1 in self.img_id]
+
+        img_id2 = [str(img_id1) for img_id1 in self.img_id]
         img_len = len(img_id2)
         img_id2 = [img_id2[sortedIndex_mat[i]] for i in range(img_len)]
         img_id20 = []
-        for mm in img_id2:
+        for mm in img_id2:#返回根目录和图片名字
             mm1 = mm.split('/')
 
             mm2 = mm1[6:9]
@@ -125,3 +125,4 @@ class image_retrieval:
 
 
         return img_id20, name
+
